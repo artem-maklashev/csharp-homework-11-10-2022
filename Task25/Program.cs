7091fbc406842bba0ffb512f4524695753b6ae4b
Artem Maklashev <artem@maklashev.ru>
2022-10-11 16:38:39 +0400

    All task complited

---
+++ Task25/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16 */
+
+int GetNumber(string message)
+{
+    int result = 0;
+    bool isCorrect = false;
+
+    while(!isCorrect)
+    {
+        Console.Write(message);
+
+        if (int.TryParse(Console.ReadLine(), out result))
+            isCorrect = true;
+        else
+           Console.WriteLine("Ошибка. Введите число ");
+    }
+    return result;
+}
+
+int GetExponentiation(int x, int y)
+{
+    int result = 1;
+    for (int i = 1; i <= y ; i++)
+        result=result*x;    
+    return result;
+}
+
+int a = GetNumber("Введите число А: ");
+int b = GetNumber("Введите число B: ");
+int exp = GetExponentiation(a,b);
+Console.WriteLine($"{a},{b} -> {exp}");
---
+++ Task27/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12 */
+
+string GetNumber(string message)
+{
+    int result = 0;
+    bool isCorrect = false;
+
+    while(!isCorrect)
+    {
+        Console.Write(message);
+
+        if (int.TryParse(Console.ReadLine(), out result))
+            isCorrect = true;
+        else
+           Console.WriteLine("Ошибка. Введите число ");
+    }
+    return result.ToString();
+}
+
+int TakeSumNumbers(string numb)
+{
+    int result = 0;
+    foreach (var i in numb)
+    {
+        result=result+ int.Parse(i.ToString());
+    }
+    return result;
+}
+
+string number = GetNumber("Введите число: ");
+int sum = TakeSumNumbers(number);
+Console.WriteLine($"{number} -> {sum}");
